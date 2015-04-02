0a4eebc35eed444073779d0e4af119db4adcc72b
Chris Keavey <chris@chriskeavey.com>
2015-04-02 23:43:29 +0100

    Begin to customise a child theme.

---
+++ wordpress/wp-content/themes/lander/front-page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The custom template for the one-page style front page. Kicks in automatically.
+ *
+ * @package Simone
+ */
+
+get_header(); ?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+      <div>Hello - Test front page.</div>
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
